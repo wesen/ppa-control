57b26245bae0af1a1d0a36a79727ba6bc96a9c1f
Manuel Odendahl <wesen@ruinwesen.com>
2022-07-17 11:38:42 -0400

    :art: Mess with canvas

--- cmd/ui-test/main.go
+++ cmd/ui-test/main.go
@@ -3,27 +3,50 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/canvas"
+	"image/color"
+	"time"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("ppa-control")
+	c := w.Canvas()
 
-	w.SetContent(container.NewVBox(makeUI()))
+	blue := color.NRGBA{R: 0, G: 0, B: 255, A: 255}
+	rect := canvas.NewRectangle(blue)
+	c.SetContent(rect)
 
-	w.Resize(fyne.NewSize(300, 300))
-	w.Show()
+	go func() {
+		time.Sleep(2 * time.Second)
+		green := color.NRGBA{R: 0, G: 255, B: 0, A: 255}
+		rect.FillColor = green
+		rect.Refresh()
 
-	a.Run()
+		time.Sleep(2 * time.Second)
+		rect.Refresh()
+		setContentToText(c)
+
+		time.Sleep(2 * time.Second)
+		rect.Refresh()
+		setContentToCircle(c)
+	}()
+
+	w.ShowAndRun()
 }
 
-func makeUI() (*widget.Label, *widget.Entry) {
-	in := widget.NewEntry()
-	out := widget.NewLabel("Hello World")
-	in.OnChanged = func(content string) {
-		out.SetText("Hello " + content)
-	}
-	return out, in
+func setContentToText(c fyne.Canvas) {
+	green := color.NRGBA{R: 0, G: 255, B: 0, A: 255}
+	text := canvas.NewText("Hello World", green)
+	text.TextStyle.Bold = true
+	c.SetContent(text)
+}
+
+func setContentToCircle(c fyne.Canvas) {
+	green := color.NRGBA{R: 0, G: 255, B: 0, A: 255}
+	circle := canvas.NewCircle(green)
+	circle.StrokeWidth = 4
+	red := color.NRGBA{R: 255, G: 0, B: 0, A: 255}
+	circle.StrokeColor = red
+	c.SetContent(circle)
 }
