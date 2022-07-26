adacb94e1432ac8a71c9a096cac54f17f5a541a7
Manuel Odendahl <wesen@ruinwesen.com>
2022-07-26 13:41:31 -0400

    :sparkles: Filter out UDP 5001 packets

--- cmd/pcap/main.go
+++ cmd/pcap/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 	"os"
 )
@@ -27,5 +29,27 @@ func main() {
 }
 
 func handlePacket(packet gopacket.Packet) {
-	fmt.Printf("%s\n", packet.String())
+	ip4Layer := packet.Layer(layers.LayerTypeIPv4)
+	if ip4Layer == nil {
+		return
+	}
+	udpLayer := packet.Layer(layers.LayerTypeUDP)
+	if udpLayer == nil {
+		return
+	}
+	payload := udpLayer.LayerPayload()
+	if payload == nil {
+		return
+	}
+	iPv4 := ip4Layer.(*layers.IPv4)
+	udp := udpLayer.(*layers.UDP)
+
+	if udp.SrcPort == 5001 || udp.DstPort == 5001 {
+		fmt.Printf("src: %s:%d dst: %s:%d\n",
+			iPv4.SrcIP,
+			udp.SrcPort,
+			iPv4.DstIP,
+			udp.DstPort)
+		fmt.Printf("%s\n", hex.Dump(payload))
+	}
 }
