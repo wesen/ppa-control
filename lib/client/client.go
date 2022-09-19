34654a3c644ef1af7c0ec2da22eeedcc367e7722
Manuel Odendahl <wesen@ruinwesen.com>
2022-09-19 09:23:05 -0400

    :art: Refactor send and read loops into separate functions

--- lib/client/client.go
+++ lib/client/client.go
@@ -95,8 +95,9 @@ func (c *SingleDevice) SendPresetRecallByPresetIndex(index int) {
 	c.SendChannel <- buf
 }
 
-// TODO we should probably pass the received message channel in here,
-// instead of making it part of the struct
+// Run is the main loop for the client.  It will listen for messages on the sendChannel
+// and emit them on the UDP socket, and it will listen for incoming packets on the UDP socket,
+// parse them and emit them on the receiveChannel.
 func (c *SingleDevice) Run(ctx context.Context, receivedCh *chan ReceivedMessage) (err error) {
 	raddr, err := net.ResolveUDPAddr("udp", c.Address)
 	if err != nil {
@@ -111,136 +112,146 @@ func (c *SingleDevice) Run(ctx context.Context, receivedCh *chan ReceivedMessage
 
 	grp, ctx := errgroup.WithContext(ctx)
 	grp.Go(func() error {
-		defer func() {
-			log.Printf("read-loop exiting\n")
-		}()
-		defer func() {
-			log.Info().
-				Str("address", conn.LocalAddr().String()).
-				Msg("Exiting read loop")
-		}()
-
-		for {
-			buffer := make([]byte, MaxBufferSize)
-			// Block on read
-			log.Debug().
-				Str("address", conn.LocalAddr().String()).
-				Msg("Reading from connection")
-
-			// TODO handle ctx cancellation
-
-			//deadline := time.Now().Add(Timeout)
-			//err = conn.SetReadDeadline(deadline)
-			//if err != nil {
-			//	log.Warn().
-			//		Str("address", conn.LocalAddr().String()).
-			//		Str("error", err.Error()).
-			//		Msg("Failed to set read deadline")
-			//	return err
-			//}
-
-			nRead, addr, err := conn.ReadFrom(buffer)
-			if err != nil {
-				switch v := err.(type) {
-				case *net.OpError:
-					switch v2 := v.Err.(type) {
-					case *os.SyscallError:
-						if v2.Syscall == "recvfrom" && v2.Err == syscall.ECONNREFUSED {
-							log.Warn().Msg("Connection refused")
-						}
-					default:
-					}
-				default:
-				}
-				log.Warn().Err(err).Msg("Failed to read from connection")
-				return err
-			}
-
-			if zerolog.GlobalLevel() == zerolog.DebugLevel {
-				fmt.Printf("%s\n", hexdump.Dump(buffer[:nRead]))
-			}
-			log.Info().Int("received", nRead).
-				Str("from", addr.String()).
-				Str("local", conn.LocalAddr().String()).
-				Bytes("data", buffer[:nRead]).
-				Msg("Received packet")
-
-			hdr, err := protocol.ParseHeader(buffer[:nRead])
-
-			if err != nil {
-				log.Warn().Err(err).
-					Bytes("payload", buffer[:nRead]).
-					Msg("Could not decode incoming message")
-
-				if receivedCh != nil {
-					*receivedCh <- ReceivedMessage{
-						Header:  nil,
-						Address: addr,
-						Body:    buffer[:nRead],
-					}
-				}
-				continue
-			}
-
-			if receivedCh != nil {
-				*receivedCh <- ReceivedMessage{
-					Header:  hdr,
-					Address: addr,
-					Body:    buffer[:nRead],
-				}
-			}
-		}
+		return c.readLoop(ctx, conn, receivedCh)
 	})
 
 	grp.Go(func() error {
-		log.Info().Str("address", c.Address).Msg("Starting send loop")
-		defer func() {
-			log.Info().Str("address", c.Address).Msg("Exiting send loop")
-		}()
-		for {
-			select {
-			case <-ctx.Done():
-				fmt.Println("SingleDevice.go: cancelled")
-				return ctx.Err()
-
-			case buf := <-c.SendChannel:
-				// Send
-				go func() {
-					log.Debug().Str("address", c.Address).Int("len", buf.Len()).Msg("Sending packet")
-					deadline := time.Now().Add(Timeout)
-					err = conn.SetWriteDeadline(deadline)
-					if err != nil {
-						log.Warn().
-							Err(err).
-							Str("address", c.Address).
-							Msg("Failed to set write deadline")
-					}
-					n, err := conn.WriteToUDP(buf.Bytes(), raddr)
-					if err != nil {
-						log.Warn().
-							Err(err).
-							Str("to", c.Address).
-							Str("local", conn.LocalAddr().String()).
-							Int("length", buf.Len()).
-							Bytes("data", buf.Bytes()).
-							Msg("Failed to write to connection")
-					} else {
-						log.Debug().
-							Str("to", c.Address).
-							Str("from", conn.LocalAddr().String()).
-							Int("length", buf.Len()).
-							Int("written", n).
-							Msg("Written packet")
-
-					}
-
-				}()
-			}
-		}
+		return c.sendLoop(ctx, conn, raddr)
 	})
 
 	log.Info().Str("address", c.Address).Msg("Client started")
 	return grp.Wait()
+}
+
+func (c *SingleDevice) sendLoop(ctx context.Context, conn *net.UDPConn, raddr *net.UDPAddr) error {
+	log.Info().Str("address", c.Address).Msg("Starting send loop")
+	defer func() {
+		log.Info().Str("address", c.Address).Msg("Exiting send loop")
+	}()
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("SingleDevice.go: cancelled")
+			return ctx.Err()
+
+		case buf := <-c.SendChannel:
+			go func() {
+				log.Debug().Str("address", c.Address).Int("len", buf.Len()).Msg("Sending packet")
+				deadline := time.Now().Add(Timeout)
+				err := conn.SetWriteDeadline(deadline)
+				if err != nil {
+					log.Warn().
+						Err(err).
+						Str("address", c.Address).
+						Msg("Failed to set write deadline")
+				}
+				n, err := conn.WriteToUDP(buf.Bytes(), raddr)
+				if err != nil {
+					log.Warn().
+						Err(err).
+						Str("to", c.Address).
+						Str("local", conn.LocalAddr().String()).
+						Int("length", buf.Len()).
+						Bytes("data", buf.Bytes()).
+						Msg("Failed to write to connection")
+				} else {
+					log.Debug().
+						Str("to", c.Address).
+						Str("from", conn.LocalAddr().String()).
+						Int("length", buf.Len()).
+						Int("written", n).
+						Msg("Written packet")
+
+				}
+			}()
+		}
+	}
+}
+
+func (c *SingleDevice) readLoop(ctx context.Context, conn *net.UDPConn, receivedCh *chan ReceivedMessage) error {
+	defer func() {
+		log.Info().
+			Str("address", conn.LocalAddr().String()).
+			Msg("Exiting read loop")
+	}()
+
+	for {
+		if ctx.Err() != nil {
+			log.Debug().Msg("Context error, exiting read loop")
+			return ctx.Err()
+		}
+
+		buffer := make([]byte, MaxBufferSize)
+		// Block on read
+		log.Debug().
+			Str("address", conn.LocalAddr().String()).
+			Msg("Reading from connection")
+
+		deadline := time.Now().Add(200 * time.Millisecond)
+		err := conn.SetReadDeadline(deadline)
+		if err != nil {
+			log.Warn().
+				Str("address", conn.LocalAddr().String()).
+				Str("error", err.Error()).
+				Msg("Failed to set read deadline")
+			return err
+		}
+
+		nRead, addr, err := conn.ReadFrom(buffer)
+		if err != nil {
+			switch v := err.(type) {
+			case *net.OpError:
+				if v.Timeout() {
+					log.Debug().Msg("Read timeout")
+					continue
+				}
+				switch v2 := v.Err.(type) {
+				case *os.SyscallError:
+					if v2.Syscall == "recvfrom" && v2.Err == syscall.ECONNREFUSED {
+						log.Warn().Msg("Connection refused")
+					}
+				default:
+				}
+			default:
+			}
+			log.Warn().Err(err).Msg("Failed to read from connection")
+			return err
+		}
+
+		if zerolog.GlobalLevel() == zerolog.DebugLevel {
+			fmt.Printf("%s\n", hexdump.Dump(buffer[:nRead]))
+		}
+		log.Info().Int("received", nRead).
+			Str("from", addr.String()).
+			Str("local", conn.LocalAddr().String()).
+			Bytes("data", buffer[:nRead]).
+			Msg("Received packet")
+
+		hdr, err := protocol.ParseHeader(buffer[:nRead])
+
+		if err != nil {
+			log.Warn().Err(err).
+				Bytes("payload", buffer[:nRead]).
+				Msg("Could not decode incoming message")
+
+			if receivedCh != nil {
+				*receivedCh <- ReceivedMessage{
+					Header:  nil,
+					Address: addr,
+					Body:    buffer[:nRead],
+				}
+			}
+			continue
+		}
+
+		if receivedCh != nil {
+			*receivedCh <- ReceivedMessage{
+				Header:  hdr,
+				Address: addr,
+				Body:    buffer[:nRead],
+			}
+		}
+	}
 }
 
 func (c *SingleDevice) Name() string {
